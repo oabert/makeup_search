1f249bfe79f96db880956256d34f6875a00e7438
Olena Abert <oabert@gmail.com>
2022-11-15 09:36:26 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
+import {Component} from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            makeups: [],
+        };
+    }
+
+    componentDidMount() {
+            fetch('http://makeup-api.herokuapp.com/api/v1/products.json')
+            .then(((response) => response.json()))
+            .then((items) =>
+                this.setState(
+                    () => {
+                        return {makeups: items};
+                    },
+                    () => {
+                        console.log(this.state);
+                    }
+                ))
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <input className='search-box'
+                       type='search'
+                       placeholder='search item'
+                       onChange={(event) => {
+                           const searchString = event.target.value.toLowerCase();
+                           const filteredItem = this.state.makeups.filter((makeup) => {
+                               return makeup.name.toLowerCase().includes(searchString)
+                           })
+                           this.setState(() => {
+                               return {makeups: filteredItem};
+                           })
+                       }}
+                />
+
+                {
+                    this.state.makeups.slice(0, 10).map((makeup) => {
+                        return (
+                            <div key={makeup.id}>
+                                <h4>{makeup.name}</h4>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
